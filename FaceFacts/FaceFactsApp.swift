2791c4b3265e855004a3364bc1cce269d5efea54
Krish Mittal <krishmittal212005@gmail.com>
2024-05-10 14:54:59 +0530

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Krish Mittal on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
